25c3c527f79bcd285dfb0dfaa056c3f2867a5242
Frederick Akalin <akalin@keyba.se>
2016-05-26 12:55:44 -0700

    Reorder

--- libkbfs/bare_tlf_handle.go
+++ libkbfs/bare_tlf_handle.go
@@ -129,72 +129,6 @@ func MakeBareTlfHandle(
 	}, nil
 }
 
-func resolveAssertions(assertions map[keybase1.SocialAssertion]keybase1.UID,
-	unresolved []keybase1.SocialAssertion, resolved []keybase1.UID) (
-	map[keybase1.UID]bool, []keybase1.SocialAssertion) {
-	resolvedMap := uidSliceToSet(resolved)
-	unresolvedMap := assertionSliceToSet(unresolved)
-	for a, u := range assertions {
-		if unresolvedMap[a] {
-			resolvedMap[u] = true
-			delete(unresolvedMap, a)
-		}
-	}
-	return resolvedMap, assertionSetToSlice(unresolvedMap)
-}
-
-func uidSetToSlice(m map[keybase1.UID]bool) (s []keybase1.UID) {
-	for u := range m {
-		s = append(s, u)
-	}
-	return s
-}
-
-func assertionSetToSlice(m map[keybase1.SocialAssertion]bool) (s []keybase1.SocialAssertion) {
-	for u := range m {
-		s = append(s, u)
-	}
-	return s
-}
-
-func uidSliceToSet(s []keybase1.UID) map[keybase1.UID]bool {
-	m := make(map[keybase1.UID]bool, len(s))
-	for _, u := range s {
-		m[u] = true
-	}
-	return m
-}
-
-func assertionSliceToSet(s []keybase1.SocialAssertion) map[keybase1.SocialAssertion]bool {
-	m := make(map[keybase1.SocialAssertion]bool, len(s))
-	for _, u := range s {
-		m[u] = true
-	}
-	return m
-}
-
-// ResolveAssertions creates a new BareTlfHandle given an existing one with
-// while resolving the passed assertions.
-func (h BareTlfHandle) ResolveAssertions(
-	assertions map[keybase1.SocialAssertion]keybase1.UID) BareTlfHandle {
-	if len(assertions) == 0 || (len(h.UnresolvedWriters) == 0 && len(h.UnresolvedReaders) == 0) {
-		return h
-	}
-	var resolvedWriters, resolvedReaders map[keybase1.UID]bool
-	resolvedWriters, h.UnresolvedWriters = resolveAssertions(assertions, h.UnresolvedWriters, h.Writers)
-	resolvedReaders, h.UnresolvedReaders = resolveAssertions(assertions, h.UnresolvedReaders, h.Readers)
-	h.Writers = uidSetToSlice(resolvedWriters)
-	for _, u := range h.Writers {
-		delete(resolvedReaders, u)
-	}
-	h.Readers = uidSetToSlice(resolvedReaders)
-	sort.Sort(uidList(h.Writers))
-	sort.Sort(uidList(h.Readers))
-	sort.Sort(socialAssertionList(h.UnresolvedWriters))
-	sort.Sort(socialAssertionList(h.UnresolvedReaders))
-	return h
-}
-
 // IsPublic returns whether or not this BareTlfHandle represents a
 // public top-level folder.
 func (h BareTlfHandle) IsPublic() bool {
@@ -248,4 +182,70 @@ func (h BareTlfHandle) UnresolvedUsers() []keybase1.SocialAssertion {
 	unresolvedUsers = append(unresolvedUsers, h.UnresolvedWriters...)
 	unresolvedUsers = append(unresolvedUsers, h.UnresolvedReaders...)
 	return unresolvedUsers
+}
+
+func uidSliceToSet(s []keybase1.UID) map[keybase1.UID]bool {
+	m := make(map[keybase1.UID]bool, len(s))
+	for _, u := range s {
+		m[u] = true
+	}
+	return m
+}
+
+func assertionSliceToSet(s []keybase1.SocialAssertion) map[keybase1.SocialAssertion]bool {
+	m := make(map[keybase1.SocialAssertion]bool, len(s))
+	for _, u := range s {
+		m[u] = true
+	}
+	return m
+}
+
+func resolveAssertions(assertions map[keybase1.SocialAssertion]keybase1.UID,
+	unresolved []keybase1.SocialAssertion, resolved []keybase1.UID) (
+	map[keybase1.UID]bool, []keybase1.SocialAssertion) {
+	resolvedMap := uidSliceToSet(resolved)
+	unresolvedMap := assertionSliceToSet(unresolved)
+	for a, u := range assertions {
+		if unresolvedMap[a] {
+			resolvedMap[u] = true
+			delete(unresolvedMap, a)
+		}
+	}
+	return resolvedMap, assertionSetToSlice(unresolvedMap)
+}
+
+func uidSetToSlice(m map[keybase1.UID]bool) (s []keybase1.UID) {
+	for u := range m {
+		s = append(s, u)
+	}
+	return s
+}
+
+func assertionSetToSlice(m map[keybase1.SocialAssertion]bool) (s []keybase1.SocialAssertion) {
+	for u := range m {
+		s = append(s, u)
+	}
+	return s
+}
+
+// ResolveAssertions creates a new BareTlfHandle given an existing one with
+// while resolving the passed assertions.
+func (h BareTlfHandle) ResolveAssertions(
+	assertions map[keybase1.SocialAssertion]keybase1.UID) BareTlfHandle {
+	if len(assertions) == 0 || (len(h.UnresolvedWriters) == 0 && len(h.UnresolvedReaders) == 0) {
+		return h
+	}
+	var resolvedWriters, resolvedReaders map[keybase1.UID]bool
+	resolvedWriters, h.UnresolvedWriters = resolveAssertions(assertions, h.UnresolvedWriters, h.Writers)
+	resolvedReaders, h.UnresolvedReaders = resolveAssertions(assertions, h.UnresolvedReaders, h.Readers)
+	h.Writers = uidSetToSlice(resolvedWriters)
+	for _, u := range h.Writers {
+		delete(resolvedReaders, u)
+	}
+	h.Readers = uidSetToSlice(resolvedReaders)
+	sort.Sort(uidList(h.Writers))
+	sort.Sort(uidList(h.Readers))
+	sort.Sort(socialAssertionList(h.UnresolvedWriters))
+	sort.Sort(socialAssertionList(h.UnresolvedReaders))
+	return h
 }
