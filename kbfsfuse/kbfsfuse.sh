e177c585edf5357b9d75dfe08b8e201538a587e8
John Zila <john@keyba.se>
2016-05-31 08:46:25 -0700

    Updated mdserver and bserver addresses to use environment variables
    Suggested by @strib
    
    Signed-off-by: John Zila <john@keyba.se>

--- kbfsfuse/kbfsfuse.sh
+++ kbfsfuse/kbfsfuse.sh
@@ -10,7 +10,7 @@ trap _term SIGTERM
 
 keybase service &
 SERVICE=$!
-kbfsfuse -debug -mdserver mdserver.kbfs.local:8125 -bserver bserver.kbfs.local:8225 -enable-sharing-before-signup /keybase &
+kbfsfuse -debug -mdserver $MDSERVER_ADDR -bserver $BSERVER_ADDR -enable-sharing-before-signup /keybase &
 KBFS=$!
 
 wait "$SERVICE"
