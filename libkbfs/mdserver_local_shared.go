24cc3635c6b0257a31ee924a15114aeae0ec9fe5
Frederick Akalin <akalin@keyba.se>
2016-06-23 18:58:32 -0700

    fix names

--- libkbfs/mdserver_local_shared.go
+++ libkbfs/mdserver_local_shared.go
@@ -77,45 +77,45 @@ func newMDServerLocalUpdateManager() *mdServerLocalUpdateManager {
 	}
 }
 
-func (s *mdServerLocalUpdateManager) setHead(id TlfID, server MDServer) {
-	s.lock.Lock()
-	defer s.lock.Unlock()
+func (m *mdServerLocalUpdateManager) setHead(id TlfID, server MDServer) {
+	m.lock.Lock()
+	defer m.lock.Unlock()
 
-	s.sessionHeads[id] = server
+	m.sessionHeads[id] = server
 
 	// now fire all the observers that aren't from this session
-	for k, v := range s.observers[id] {
+	for k, v := range m.observers[id] {
 		if k != server {
 			v <- nil
 			close(v)
-			delete(s.observers[id], k)
+			delete(m.observers[id], k)
 		}
 	}
-	if len(s.observers[id]) == 0 {
-		delete(s.observers, id)
+	if len(m.observers[id]) == 0 {
+		delete(m.observers, id)
 	}
 }
 
-func (s *mdServerLocalUpdateManager) registerForUpdate(
+func (m *mdServerLocalUpdateManager) registerForUpdate(
 	id TlfID, currHead, currMergedHeadRev MetadataRevision,
 	server MDServer) <-chan error {
-	s.lock.Lock()
-	defer s.lock.Unlock()
+	m.lock.Lock()
+	defer m.lock.Unlock()
 
 	c := make(chan error, 1)
-	if currMergedHeadRev > currHead && server != s.sessionHeads[id] {
+	if currMergedHeadRev > currHead && server != m.sessionHeads[id] {
 		c <- nil
 		close(c)
 		return c
 	}
 
-	if _, ok := s.observers[id]; !ok {
-		s.observers[id] = make(map[MDServer]chan<- error)
+	if _, ok := m.observers[id]; !ok {
+		m.observers[id] = make(map[MDServer]chan<- error)
 	}
 
 	// Otherwise, this is a legit observer.  This assumes that each
 	// client will be using a unique instance of MDServerLocal.
-	if _, ok := s.observers[id][server]; ok {
+	if _, ok := m.observers[id][server]; ok {
 		// If the local node registers something twice, it indicates a
 		// fatal bug.  Note that in the real MDServer implementation,
 		// we should allow this, in order to make the RPC properly
@@ -123,6 +123,6 @@ func (s *mdServerLocalUpdateManager) registerForUpdate(
 		panic(fmt.Errorf("Attempted double-registration for MDServerLocal %v",
 			server))
 	}
-	s.observers[id][server] = c
+	m.observers[id][server] = c
 	return c
 }
