f478cd604d1d4f6627a9e2b9f1c81e9c1770b610
Frederick Akalin <akalin@keyba.se>
2016-08-01 13:23:32 -0700

    Add (failing) test

--- libkbfs/journal_block_server_test.go
+++ libkbfs/journal_block_server_test.go
@@ -81,3 +81,64 @@ func TestJournalBlockServerPutGetAddReference(t *testing.T) {
 	require.Equal(t, data, buf)
 	require.Equal(t, serverHalf, key)
 }
+
+func TestJournalBlockServerRemoveBlockReferences(t *testing.T) {
+	// setup
+	tempdir, err := ioutil.TempDir(os.TempDir(), "journal_block_server")
+	require.NoError(t, err)
+	defer func() {
+		err := os.RemoveAll(tempdir)
+		require.NoError(t, err)
+	}()
+
+	config := MakeTestConfigOrBust(t, "test_user")
+	defer CheckConfigAndShutdown(t, config)
+
+	// Use a shutdown-only BlockServer so that it errors if the
+	// journal tries to access it.
+	config.SetBlockServer(shutdownOnlyBlockServer{})
+	log := config.MakeLogger("")
+	jServer := makeJournalServer(
+		config, log, tempdir, config.BlockServer(), config.MDOps())
+	config.SetBlockServer(jServer.blockServer())
+	config.SetMDOps(jServer.mdOps())
+
+	tlfID := FakeTlfID(2, false)
+	err = jServer.Enable(tlfID)
+	require.NoError(t, err)
+
+	ctx := context.Background()
+	blockServer := config.BlockServer()
+	crypto := config.Crypto()
+
+	uid1 := keybase1.MakeTestUID(1)
+	bCtx := BlockContext{uid1, "", zeroBlockRefNonce}
+	data := []byte{1, 2, 3, 4}
+	bID, err := crypto.MakePermanentBlockID(data)
+	require.NoError(t, err)
+
+	// Put a block.
+	serverHalf, err := crypto.MakeRandomBlockCryptKeyServerHalf()
+	require.NoError(t, err)
+	err = blockServer.Put(ctx, bID, tlfID, bCtx, data, serverHalf)
+	require.NoError(t, err)
+
+	// Add a reference.
+	uid2 := keybase1.MakeTestUID(2)
+	nonce, err := crypto.MakeBlockRefNonce()
+	require.NoError(t, err)
+	bCtx2 := BlockContext{uid1, uid2, nonce}
+	err = blockServer.AddBlockReference(ctx, bID, tlfID, bCtx2)
+	require.NoError(t, err)
+
+	// Remove the references, including a non-existent one.
+	nonce2, err := crypto.MakeBlockRefNonce()
+	require.NoError(t, err)
+	bCtx3 := BlockContext{uid1, uid2, nonce2}
+	liveCounts, err := blockServer.RemoveBlockReferences(
+		ctx, tlfID, map[BlockID][]BlockContext{
+			bID: {bCtx, bCtx2, bCtx3},
+		})
+	require.NoError(t, err)
+	require.Equal(t, map[BlockID]int{bID: 0}, liveCounts)
+}
