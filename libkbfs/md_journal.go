9dcac07771e62f47152953079f5a3ef15959805b
Frederick Akalin <akalin@keyba.se>
2016-10-14 14:36:41 -0700

    Update comments and paths

--- libkbfs/md_journal.go
+++ libkbfs/md_journal.go
@@ -68,6 +68,12 @@ func MakeImmutableBareRootMetadata(
 // ...
 // dir/mds/01ff/f...ff/data
 // dir/mds/01ff/f...ff/info.json
+// dir/wkbv3/0100...01
+// ...
+// dir/wkbv3/0100...ff
+// dir/rkbv3/0100...01
+// ...
+// dir/rkbv3/0100...ff
 //
 // There's a single journal subdirectory; the journal ordinals are
 // just MetadataRevisions, and the journal entries are just MdIDs.
@@ -85,6 +91,10 @@ func MakeImmutableBareRootMetadata(
 // add more files to this directory; if any code is written to move
 // MDs around, it should be careful to preserve any unknown files in
 // an MD directory.
+//
+// Writer (reader) key bundles for V3 metadata objects are stored
+// separately in dir/wkbv3 (dir/rkbv3). The number of bundles is
+// small, so no need to splay them.
 //
 // The maximum number of characters added to the root dir by an MD
 // journal is 50:
@@ -194,22 +204,26 @@ func (j mdJournal) mdsPath() string {
 	return filepath.Join(j.dir, "mds")
 }
 
+// The final components of the paths below are truncated to 34
+// characters, which corresponds to 16 random bytes (since the first
+// byte is a hash type) or 128 random bits, which means that the
+// expected number of MDs generated before getting a path collision is
+// 2^64 (see
+// https://en.wikipedia.org/wiki/Birthday_problem#Cast_as_a_collision_problem
+// ). The full ID can be recovered just by hashing the data again with
+// the same hash type.
+
 func (j mdJournal) writerKeyBundleV3Path(id TLFWriterKeyBundleID) string {
-	return filepath.Join(j.dir, "wkbv3", id.String())
+	idStr := id.String()
+	return filepath.Join(j.dir, "wkbv3", idStr[:34])
 }
 
 func (j mdJournal) readerKeyBundleV3Path(id TLFReaderKeyBundleID) string {
-	return filepath.Join(j.dir, "rkbv3", id.String())
+	idStr := id.String()
+	return filepath.Join(j.dir, "rkbv3", idStr[:34])
 }
 
 func (j mdJournal) mdPath(id MdID) string {
-	// Truncate to 34 characters, which corresponds to 16 random
-	// bytes (since the first byte is a hash type) or 128 random
-	// bits, which means that the expected number of MDs generated
-	// before getting a path collision is 2^64 (see
-	// https://en.wikipedia.org/wiki/Birthday_problem#Cast_as_a_collision_problem
-	// ). The full ID can be recovered just by hashing the data
-	// again with the same hash type.
 	idStr := id.String()
 	return filepath.Join(j.mdsPath(), idStr[:4], idStr[4:34])
 }
