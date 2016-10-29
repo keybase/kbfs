77c836218bbb6cce861b98912cfc6ca2e19c970c
Frederick Akalin <akalin@keyba.se>
2016-10-29 12:57:44 -0700

    Split off

--- libkbfs/bare_root_metadata_v2_test.go
+++ libkbfs/bare_root_metadata_v2_test.go
@@ -5,9 +5,12 @@
 package libkbfs
 
 import (
+	"context"
 	"testing"
 
 	"github.com/keybase/client/go/protocol/keybase1"
+	"github.com/keybase/kbfs/kbfscodec"
+	"github.com/keybase/kbfs/kbfscrypto"
 	"github.com/stretchr/testify/require"
 )
 
@@ -43,4 +46,55 @@ func TestRootMetadataVersionV2(t *testing.T) {
 
 	rmd.SetUnresolvedWriters(nil)
 	require.Equal(t, PreExtraMetadataVer, rmd.Version())
+}
+
+func TestIsValidRekeyRequestBasicV2(t *testing.T) {
+	tlfID := FakeTlfID(1, false)
+
+	uid := keybase1.MakeTestUID(1)
+	bh, err := MakeBareTlfHandle([]keybase1.UID{uid}, nil, nil, nil, nil)
+	require.NoError(t, err)
+
+	brmd, err := MakeInitialBareRootMetadataV2(tlfID, bh)
+
+	ctx := context.Background()
+	codec := kbfscodec.NewMsgpack()
+	signer := kbfscrypto.SigningKeySigner{
+		Key: MakeFakeSigningKeyOrBust("key1"),
+	}
+
+	err = brmd.SignWriterMetadataInternally(ctx, codec, signer)
+	require.NoError(t, err)
+
+	newBrmd, err := MakeInitialBareRootMetadataV2(tlfID, bh)
+	require.NoError(t, err)
+	ok, err := newBrmd.IsValidRekeyRequest(
+		codec, brmd, newBrmd.LastModifyingWriter(), nil, nil)
+	require.NoError(t, err)
+	// Should fail because the copy bit is unset.
+	require.False(t, ok)
+
+	// Set the copy bit; note the writer metadata is the same.
+	newBrmd.SetWriterMetadataCopiedBit()
+
+	signer2 := kbfscrypto.SigningKeySigner{
+		Key: MakeFakeSigningKeyOrBust("key2"),
+	}
+
+	err = newBrmd.SignWriterMetadataInternally(ctx, codec, signer2)
+	require.NoError(t, err)
+
+	ok, err = newBrmd.IsValidRekeyRequest(
+		codec, brmd, newBrmd.LastModifyingWriter(), nil, nil)
+	require.NoError(t, err)
+	// Should fail because of mismatched writer metadata siginfo.
+	require.False(t, ok)
+
+	// Re-sign to get the same signature.
+	err = newBrmd.SignWriterMetadataInternally(ctx, codec, signer)
+	require.NoError(t, err)
+	ok, err = newBrmd.IsValidRekeyRequest(
+		codec, brmd, newBrmd.LastModifyingWriter(), nil, nil)
+	require.NoError(t, err)
+	require.True(t, ok)
 }
--- libkbfs/bare_root_metadata_v3_test.go
+++ libkbfs/bare_root_metadata_v3_test.go
@@ -8,6 +8,8 @@ import (
 	"testing"
 
 	"github.com/keybase/client/go/protocol/keybase1"
+	"github.com/keybase/kbfs/kbfscodec"
+	"github.com/keybase/kbfs/kbfscrypto"
 	"github.com/stretchr/testify/require"
 )
 
@@ -24,4 +26,38 @@ func TestRootMetadataVersionV3(t *testing.T) {
 	require.NoError(t, err)
 
 	require.Equal(t, SegregatedKeyBundlesVer, rmd.Version())
+}
+
+func TestIsValidRekeyRequestBasicV3(t *testing.T) {
+	tlfID := FakeTlfID(1, false)
+
+	uid := keybase1.MakeTestUID(1)
+	bh, err := MakeBareTlfHandle([]keybase1.UID{uid}, nil, nil, nil, nil)
+	require.NoError(t, err)
+
+	codec := kbfscodec.NewMsgpack()
+	crypto := MakeCryptoCommon(kbfscodec.NewMsgpack())
+
+	brmd, err := MakeInitialBareRootMetadataV3(tlfID, bh)
+	extra, err := FakeInitialRekey(
+		brmd, crypto, bh, kbfscrypto.TLFPublicKey{})
+
+	newBrmd, err := MakeInitialBareRootMetadataV3(tlfID, bh)
+	require.NoError(t, err)
+	newExtra, err := FakeInitialRekey(
+		newBrmd, crypto, bh, kbfscrypto.TLFPublicKey{})
+
+	ok, err := newBrmd.IsValidRekeyRequest(
+		codec, brmd, newBrmd.LastModifyingWriter(), extra, newExtra)
+	require.NoError(t, err)
+	// Should fail because the copy bit is unset.
+	require.False(t, ok)
+
+	// Set the copy bit; note the writer metadata is the same.
+	newBrmd.SetWriterMetadataCopiedBit()
+
+	ok, err = newBrmd.IsValidRekeyRequest(
+		codec, brmd, newBrmd.LastModifyingWriter(), extra, newExtra)
+	require.NoError(t, err)
+	require.True(t, ok)
 }
