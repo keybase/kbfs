6a03ab394cde340c322bd20983bfd372e35e5141
Frederick Akalin <akalin@keyba.se>
2016-12-02 15:28:05 -0800

    Rem

--- libkbfs/key_bundle.go
+++ libkbfs/key_bundle.go
@@ -35,85 +35,11 @@ type TLFCryptKeyInfo struct {
 }
 
 // DeviceKeyInfoMap is a map from a user devices (identified by the
-// KID of the corresponding device CryptPublicKey) to the
-// TLF's symmetric secret key information.
+// corresponding device CryptPublicKey) to the TLF's symmetric secret
+// key information.
 type DeviceKeyInfoMap map[keybase1.KID]TLFCryptKeyInfo
-
-func (kim DeviceKeyInfoMap) fillInDeviceInfo(crypto Crypto,
-	uid keybase1.UID, tlfCryptKey kbfscrypto.TLFCryptKey,
-	ePrivKey kbfscrypto.TLFEphemeralPrivateKey, ePubIndex int,
-	publicKeys []kbfscrypto.CryptPublicKey) (
-	serverMap map[keybase1.KID]kbfscrypto.TLFCryptKeyServerHalf,
-	err error) {
-	serverMap = make(map[keybase1.KID]kbfscrypto.TLFCryptKeyServerHalf)
-	// for each device:
-	//    * create a new random server half
-	//    * mask it with the key to get the client half
-	//    * encrypt the client half
-	//
-	// TODO: parallelize
-	for _, k := range publicKeys {
-		// Skip existing entries, only fill in new ones
-		if _, ok := kim[k.KID()]; ok {
-			continue
-		}
-
-		var serverHalf kbfscrypto.TLFCryptKeyServerHalf
-		serverHalf, err = crypto.MakeRandomTLFCryptKeyServerHalf()
-		if err != nil {
-			return nil, err
-		}
-
-		var clientHalf kbfscrypto.TLFCryptKeyClientHalf
-		clientHalf, err = crypto.MaskTLFCryptKey(serverHalf, tlfCryptKey)
-		if err != nil {
-			return nil, err
-		}
-
-		var encryptedClientHalf EncryptedTLFCryptKeyClientHalf
-		encryptedClientHalf, err =
-			crypto.EncryptTLFCryptKeyClientHalf(ePrivKey, k, clientHalf)
-		if err != nil {
-			return nil, err
-		}
-
-		var serverHalfID TLFCryptKeyServerHalfID
-		serverHalfID, err =
-			crypto.GetTLFCryptKeyServerHalfID(uid, k.KID(), serverHalf)
-		if err != nil {
-			return nil, err
-		}
-
-		kim[k.KID()] = TLFCryptKeyInfo{
-			ClientHalf:   encryptedClientHalf,
-			ServerHalfID: serverHalfID,
-			EPubKeyIndex: ePubIndex,
-		}
-		serverMap[k.KID()] = serverHalf
-	}
-
-	return serverMap, nil
-}
-
-func (kim DeviceKeyInfoMap) deepCopy() DeviceKeyInfoMap {
-	kimCopy := make(DeviceKeyInfoMap)
-	for kid, info := range kim {
-		// TODO: This actually still shares some data (in byte
-		// slices). Fix that.
-		kimCopy[kid] = info
-	}
-	return kimCopy
-}
 
 // UserDeviceKeyInfoMap maps a user's keybase UID to their DeviceKeyInfoMap
 type UserDeviceKeyInfoMap map[keybase1.UID]DeviceKeyInfoMap
-
-func (dkim UserDeviceKeyInfoMap) deepCopy() UserDeviceKeyInfoMap {
-	dkimCopy := make(UserDeviceKeyInfoMap)
-	for uid, kim := range dkim {
-		dkimCopy[uid] = kim.deepCopy()
-	}
-	return dkimCopy
-}
 
 type serverKeyMap map[keybase1.UID]map[keybase1.KID]kbfscrypto.TLFCryptKeyServerHalf
