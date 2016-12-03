95280982e83e592d47d74d6bceb32a3af5c43332
Frederick Akalin <akalin@keyba.se>
2016-12-03 12:47:45 -0800

    Fix test

--- libkbfs/key_bundle_test.go
+++ libkbfs/key_bundle_test.go
@@ -5,6 +5,7 @@
 package libkbfs
 
 import (
+	"strings"
 	"testing"
 
 	"github.com/keybase/client/go/protocol/keybase1"
@@ -52,9 +53,11 @@ func TestTLFCryptKeyInfoUnknownFields(t *testing.T) {
 	testStructUnknownFields(t, makeFakeTLFCryptKeyInfoFuture(t))
 }
 
-func TestUserServerHalfRemovalInfoSuccess(t *testing.T) {
+func TestUserServerHalfRemovalInfo(t *testing.T) {
 	key1 := kbfscrypto.MakeFakeCryptPublicKeyOrBust("key1")
 	key2 := kbfscrypto.MakeFakeCryptPublicKeyOrBust("key2")
+	key3 := kbfscrypto.MakeFakeCryptPublicKeyOrBust("key3")
+	key4 := kbfscrypto.MakeFakeCryptPublicKeyOrBust("key4")
 
 	half1a := kbfscrypto.MakeTLFCryptKeyServerHalf([32]byte{0x1})
 	half1b := kbfscrypto.MakeTLFCryptKeyServerHalf([32]byte{0x2})
@@ -79,23 +82,76 @@ func TestUserServerHalfRemovalInfoSuccess(t *testing.T) {
 	id2c, err := crypto.GetTLFCryptKeyServerHalfID(uid, key2.KID(), half2c)
 	require.NoError(t, err)
 
-	info := userServerHalfRemovalInfo{
-		userRemoved: true,
-		deviceServerHalfIDs: deviceServerHalfRemovalInfo{
-			key1: {id1a, id1b},
-			key2: {id2a, id2b},
-		},
+	// Required because addGeneration may modify its object even
+	// if it returns an error.
+	makeInfo := func(good bool) userServerHalfRemovalInfo {
+		var key2IDs []TLFCryptKeyServerHalfID
+		if good {
+			key2IDs = []TLFCryptKeyServerHalfID{id2a, id2b}
+		} else {
+			key2IDs = []TLFCryptKeyServerHalfID{id2a}
+		}
+		return userServerHalfRemovalInfo{
+			userRemoved: true,
+			deviceServerHalfIDs: deviceServerHalfRemovalInfo{
+				key1: {id1a, id1b},
+				key2: key2IDs,
+			},
+		}
 	}
 
 	genInfo := userServerHalfRemovalInfo{
-		userRemoved: true,
-		deviceServerHalfIDs: deviceServerHalfRemovalInfo{
-			key1: {id1c},
-			key2: {id2c},
-		},
+		userRemoved: false,
 	}
 
-	info.addGeneration(uid, genInfo)
+	err = makeInfo(true).addGeneration(uid, genInfo)
+	require.Error(t, err)
+	require.True(t, strings.HasPrefix(err.Error(), "userRemoved=true"),
+		"err=%v", err)
+
+	genInfo.userRemoved = true
+	err = makeInfo(true).addGeneration(uid, genInfo)
+	require.Error(t, err)
+	require.True(t, strings.HasPrefix(err.Error(), "device count=2"),
+		"err=%v", err)
+
+	genInfo.deviceServerHalfIDs = deviceServerHalfRemovalInfo{
+		key1: {id1c},
+		key2: {id2c},
+	}
+	err = makeInfo(false).addGeneration(uid, genInfo)
+	require.Error(t, err)
+	require.True(t,
+		// Required because of go's random iteration order.
+		strings.HasPrefix(err.Error(), "expected 2 keys") ||
+			strings.HasPrefix(err.Error(), "expected 1 keys"),
+		"err=%v", err)
+
+	genInfo.deviceServerHalfIDs = deviceServerHalfRemovalInfo{
+		key1: {},
+		key2: {},
+	}
+	err = makeInfo(true).addGeneration(uid, genInfo)
+	require.Error(t, err)
+	require.True(t, strings.HasPrefix(err.Error(),
+		"expected exactly one key"), "err=%v", err)
+
+	genInfo.deviceServerHalfIDs = deviceServerHalfRemovalInfo{
+		key3: {id1c},
+		key4: {id2c},
+	}
+	err = makeInfo(true).addGeneration(uid, genInfo)
+	require.Error(t, err)
+	require.True(t, strings.HasPrefix(err.Error(),
+		"no generation info"), "err=%v", err)
+
+	genInfo.deviceServerHalfIDs = deviceServerHalfRemovalInfo{
+		key1: {id1c},
+		key2: {id2c},
+	}
+	info := makeInfo(true)
+	err = info.addGeneration(uid, genInfo)
+	require.NoError(t, err)
 	require.Equal(t, userServerHalfRemovalInfo{
 		userRemoved: true,
 		deviceServerHalfIDs: deviceServerHalfRemovalInfo{
