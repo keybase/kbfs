a6c35ac3ae3b30c8d6739c79053e1378fefbc7d8
Frederick Akalin <akalin@keyba.se>
2016-12-22 17:59:01 -0600

    Add note

--- libkbfs/interfaces.go
+++ libkbfs/interfaces.go
@@ -1815,6 +1815,9 @@ type MutableBareRootMetadata interface {
 	//
 	// AddKeyGeneration must only be called on metadata for
 	// private TLFs.
+	//
+	// Note that the TLFPrivateKey corresponding to privKey must
+	// also be stored in PrivateMetadata.
 	AddKeyGeneration(codec kbfscodec.Codec, crypto cryptoPure,
 		currExtra ExtraMetadata,
 		updatedWriterKeys, updatedReaderKeys UserDevicePublicKeys,
