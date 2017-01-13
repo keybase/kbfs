4c87eeee657f93eb102b57b9c8361f76af22de9b
Frederick Akalin <akalin@keyba.se>
2017-01-13 14:29:56 -0800

    Remove kid

--- libkbfs/mdserver_disk.go
+++ libkbfs/mdserver_disk.go
@@ -505,7 +505,7 @@ func (md *MDServerDisk) TruncateLock(ctx context.Context, id tlf.ID) (
 		return false, err
 	}
 
-	return md.truncateLockManager.truncateLock(session.CryptPublicKey.KID(), id)
+	return md.truncateLockManager.truncateLock(session.CryptPublicKey, id)
 }
 
 // TruncateUnlock implements the MDServer interface for MDServerDisk.
@@ -527,7 +527,7 @@ func (md *MDServerDisk) TruncateUnlock(ctx context.Context, id tlf.ID) (
 		return false, err
 	}
 
-	return md.truncateLockManager.truncateUnlock(session.CryptPublicKey.KID(), id)
+	return md.truncateLockManager.truncateUnlock(session.CryptPublicKey, id)
 }
 
 // Shutdown implements the MDServer interface for MDServerDisk.
