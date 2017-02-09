3e2df7068c849a30a6ed90ea6b24d9d10b1048d6
Frederick Akalin <akalin@keyba.se>
2017-02-09 12:48:36 -0800

    More ctx plumbing

--- libkbfs/config_local.go
+++ libkbfs/config_local.go
@@ -831,7 +831,7 @@ func (c *ConfigLocal) Shutdown(ctx context.Context) error {
 	c.MDServer().Shutdown()
 	c.KeyServer().Shutdown()
 	c.KeybaseService().Shutdown()
-	c.BlockServer().Shutdown()
+	c.BlockServer().Shutdown(ctx)
 	c.Crypto().Shutdown()
 	c.Reporter().Shutdown()
 	err = c.DirtyBlockCache().Shutdown()
