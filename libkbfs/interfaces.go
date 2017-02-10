1391ce2fbfee5b3b46ecccc3cfefdfa2dc166424
John Zila <john@keyba.se>
2017-02-10 11:27:41 -0500

    bserver_remote: Refactor to use a config

--- libkbfs/interfaces.go
+++ libkbfs/interfaces.go
@@ -47,6 +47,18 @@ type cryptoPureGetter interface {
 	cryptoPure() cryptoPure
 }
 
+type cryptoGetter interface {
+	Crypto() Crypto
+}
+
+type currentInfoGetterGetter interface {
+	currentInfoGetter() currentInfoGetter
+}
+
+type signerGetter interface {
+	Signer() kbfscrypto.Signer
+}
+
 // Block just needs to be (de)serialized using msgpack
 type Block interface {
 	dataVersioner
@@ -1471,6 +1483,9 @@ type Config interface {
 	codecGetter
 	cryptoPureGetter
 	keyGetterGetter
+	cryptoGetter
+	signerGetter
+	currentInfoGetterGetter
 	KBFSOps() KBFSOps
 	SetKBFSOps(KBFSOps)
 	KBPKI() KBPKI
@@ -1488,7 +1503,6 @@ type Config interface {
 	SetBlockCache(BlockCache)
 	DirtyBlockCache() DirtyBlockCache
 	SetDirtyBlockCache(DirtyBlockCache)
-	Crypto() Crypto
 	SetCrypto(Crypto)
 	SetCodec(kbfscodec.Codec)
 	MDOps() MDOps
