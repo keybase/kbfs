b61e774ab967dc52c3af879fbd93c04fc1493d1a
Frederick Akalin <akalin@keyba.se>
2017-02-13 20:38:59 -0800

    Fix bug

--- libkbfs/backpressure_disk_limiter.go
+++ libkbfs/backpressure_disk_limiter.go
@@ -442,6 +442,7 @@ func (bdl *backpressureDiskLimiter) beforeBlockPut(
 	defer func() {
 		if err != nil {
 			bdl.byteTracker.afterBlockPut(blockBytes, false)
+			availableBytes = bdl.byteTracker.semaphore.Count()
 		}
 	}()
 
