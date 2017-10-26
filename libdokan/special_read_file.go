ac904d5dbcf5eff2be3db1fd0b16b2602018bc0b
Song Gao <song@gao.io>
2017-10-25 20:19:29 -0700

    review feedback

--- libdokan/special_read_file.go
+++ libdokan/special_read_file.go
@@ -5,6 +5,7 @@
 package libdokan
 
 import (
+	"sync"
 	"time"
 
 	"github.com/keybase/kbfs/dokan"
@@ -17,12 +18,44 @@ type SpecialReadFile struct {
 	read func(context.Context) ([]byte, time.Time, error)
 	fs   *FS
 	emptyFile
+
+	dataLock   sync.Mutex
+	data       []byte
+	ts         time.Time
+	dataExpire time.Time
+}
+
+// Docker tests poll on special files with a 1s interval, so we make the valid
+// duration 0.5s here to make docker tests refresh on every read.
+const specialReadFileValidDuration = time.Second / 2
+
+// refreshCachedDataIfNeeded reads and caches the data, which expires in
+// specialReadFileValidDuration. This exists because GetFileInformation needs
+// to call read() to get the actual size for the file. If application calls
+// GetFileInformation before ReadFile, and use the size information from the
+// former, we need to make sure the data returned from latter has a size
+// consistent with the former. This mechanism is a best effort solution for
+// that.
+func (f *SpecialReadFile) refreshCachedDataIfNeeded(ctx context.Context) (
+	data []byte, ts time.Time, err error) {
+	f.dataLock.Lock()
+	defer f.dataLock.Unlock()
+	t := time.Now()
+	if t.Before(f.dataExpire) {
+		return f.data, f.ts, nil
+	}
+	f.data, f.ts, err = f.read(ctx)
+	if err != nil {
+		return nil, time.Time{}, err
+	}
+	f.dataExpire = t.Add(specialReadFileValidDuration)
+	return f.data, f.ts, nil
 }
 
 // GetFileInformation does stats for dokan.
 func (f *SpecialReadFile) GetFileInformation(ctx context.Context, fi *dokan.FileInfo) (*dokan.Stat, error) {
 	f.fs.logEnter(ctx, "SpecialReadFile GetFileInformation")
-	data, t, err := f.read(ctx)
+	data, ts, err := f.refreshCachedDataIfNeeded(ctx)
 	if err != nil {
 		return nil, err
 	}
@@ -36,16 +69,16 @@ func (f *SpecialReadFile) GetFileInformation(ctx context.Context, fi *dokan.File
 	}
 	a.FileAttributes |= dokan.FileAttributeReadonly
 	a.FileSize = int64(len(data))
-	a.LastWrite = t
-	a.LastAccess = t
-	a.Creation = t
+	a.LastWrite = ts
+	a.LastAccess = ts
+	a.Creation = ts
 	return a, nil
 }
 
 // ReadFile does reads for dokan.
 func (f *SpecialReadFile) ReadFile(ctx context.Context, fi *dokan.FileInfo, bs []byte, offset int64) (int, error) {
 	f.fs.logEnter(ctx, "SpecialReadFile ReadFile")
-	data, _, err := f.read(ctx)
+	data, _, err := f.refreshCachedDataIfNeeded(ctx)
 	if err != nil {
 		return 0, err
 	}
--- libfuse/special_read_file.go
+++ libfuse/special_read_file.go
@@ -25,32 +25,38 @@ type SpecialReadFile struct {
 }
 
 // Have a low non-zero value for Valid to avoid being swamped with requests,
-// while still keeping the size up to date.
+// while still keeping the size up to date. Docker tests poll on special files
+// with a 1s interval, so we make the valid duration 0.5s here to make docker
+// tests refresh on every read.
 const specialReadFileValidDuration = time.Second / 2
 
 var _ fs.Node = (*SpecialReadFile)(nil)
 
-func (f *SpecialReadFile) cachedDataExpiredLocked() bool {
-	return time.Now().After(f.dataExpire)
-}
-
-func (f *SpecialReadFile) refreshCachedDataIfNeeded(ctx context.Context) (err error) {
+// refreshCachedDataIfNeeded reads and caches the data, which expires in
+// specialReadFileValidDuration. This exists because Attr needs to call read()
+// to get the actual size for the file. If application calls Attr before Open,
+// and use the size information from the former, we need to make sure the data
+// returned from latter has a size consistent with the former. This mechanism
+// is a best effort solution for that.
+func (f *SpecialReadFile) refreshCachedDataIfNeeded(ctx context.Context) (
+	data []byte, ts time.Time, err error) {
 	f.dataLock.Lock()
 	defer f.dataLock.Unlock()
-	if time.Now().Before(f.dataExpire) {
-		return nil
+	t := time.Now()
+	if t.Before(f.dataExpire) {
+		return f.data, f.ts, nil
 	}
 	f.data, f.ts, err = f.read(ctx)
 	if err != nil {
-		return err
+		return nil, time.Time{}, err
 	}
-	f.dataExpire = time.Now().Add(specialReadFileValidDuration)
-	return nil
+	f.dataExpire = t.Add(specialReadFileValidDuration)
+	return f.data, f.ts, nil
 }
 
 // Attr implements the fs.Node interface for SpecialReadFile.
 func (f *SpecialReadFile) Attr(ctx context.Context, a *fuse.Attr) error {
-	err := f.refreshCachedDataIfNeeded(ctx)
+	data, ts, err := f.refreshCachedDataIfNeeded(ctx)
 	if err != nil {
 		return err
 	}
@@ -59,9 +65,9 @@ func (f *SpecialReadFile) Attr(ctx context.Context, a *fuse.Attr) error {
 	// Some apps (e.g., Chrome) get confused if we use a 0 size
 	// here, as is usual for pseudofiles. So return the actual
 	// size, even though it may be racy.
-	a.Size = uint64(len(f.data))
-	a.Mtime = f.ts
-	a.Ctime = f.ts
+	a.Size = uint64(len(data))
+	a.Mtime = ts
+	a.Ctime = ts
 	a.Mode = 0444
 	return nil
 }
@@ -71,11 +77,11 @@ var _ fs.NodeOpener = (*SpecialReadFile)(nil)
 // Open implements the fs.NodeOpener interface for SpecialReadFile.
 func (f *SpecialReadFile) Open(ctx context.Context, req *fuse.OpenRequest,
 	resp *fuse.OpenResponse) (fs.Handle, error) {
-	err := f.refreshCachedDataIfNeeded(ctx)
+	data, _, err := f.refreshCachedDataIfNeeded(ctx)
 	if err != nil {
 		return nil, err
 	}
 
 	resp.Flags |= fuse.OpenDirectIO
-	return fs.DataHandle(f.data), nil
+	return fs.DataHandle(data), nil
 }
