5b63969c360c3dad2f8c4de04061649bc9bacae8
Frederick Akalin <akalin@keyba.se>
2017-11-06 19:47:01 -0800

    Filter a few

--- libfuse/errors.go
+++ libfuse/errors.go
@@ -4,6 +4,35 @@
 
 package libfuse
 
+import (
+	"syscall"
+
+	"bazil.org/fuse"
+	"github.com/keybase/kbfs/kbfsblock"
+	"github.com/keybase/kbfs/kbfsmd"
+)
+
+type errorWithErrno struct {
+	error
+	errno syscall.Errno
+}
+
+var _ fuse.ErrorNumber = errorWithErrno{}
+
+func (e errorWithErrno) Errno() fuse.Errno {
+	return fuse.Errno(e.errno)
+}
+
 func filterError(err error) error {
+	switch err.(type) {
+	case kbfsblock.ServerErrorUnauthorized:
+		return errorWithErrno{err, syscall.EACCES}
+	case kbfsmd.ServerErrorUnauthorized:
+		return errorWithErrno{err, syscall.EACCES}
+	case kbfsmd.ServerErrorWriteAccess:
+		return errorWithErrno{err, syscall.EACCES}
+	case kbfsmd.MetadataIsFinalError:
+		return errorWithErrno{err, syscall.EACCES}
+	}
 	return err
 }
