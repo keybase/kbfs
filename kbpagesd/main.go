21dc48ef31938eb482852041bc3553071d9233c8
Song Gao <song@gao.io>
2018-03-06 13:40:50 -0800

    make it 32 log files

--- kbpagesd/main.go
+++ kbpagesd/main.go
@@ -119,7 +119,7 @@ func main() {
 	params.EnableJournal = true
 	params.Debug = true
 	params.LogFileConfig.Path = fKBFSLogFile
-	params.LogFileConfig.MaxKeepFiles = 16
+	params.LogFileConfig.MaxKeepFiles = 32
 	// Enable simpleFS in case we need to debug.
 	params.CreateSimpleFSInstance = simplefs.NewSimpleFS
 	kbfsLog, err := libkbfs.InitLog(params, kbCtx)
